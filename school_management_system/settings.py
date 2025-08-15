179cf5e2da64ca342774240fa12461f50e11c867
Emmanuel <Aghason.emmanuel@gmail.com>
2025-08-15 02:28:25 +0000

    Some changes

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -1,5 +1,5 @@
 
-import os 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -103,6 +103,7 @@ DATABASES = {
         'HOST': 'emmidev.mysql.pythonanywhere-services.com',
         'PORT': '3306',
         'OPTIONS': {
+            'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
 
